a135dce00426186f0380d9c11fd8e33f1f32b03d
JJ <jonjones320@gmail.com>
2024-07-03 07:51:04 -0700

    Added database table 'industries' and provided data. Added join table: 'fields', and joined companies and industries.

--- data.sql
+++ data.sql
@@ -6,6 +6,8 @@ CREATE DATABASE biztimedb;
 
 DROP TABLE IF EXISTS invoices;
 DROP TABLE IF EXISTS companies;
+DROP TABLE IF EXISTS industries;
+DROP TABLE IF EXISTS fields;
 
 CREATE TABLE companies (
     code text PRIMARY KEY,
@@ -23,12 +25,36 @@ CREATE TABLE invoices (
     CONSTRAINT invoices_amt_check CHECK ((amt > (0)::double precision))
 );
 
+CREATE TABLE industries (
+  code text PRIMARY KEY,
+  industry text NOT NULL UNIQUE
+);
+
+CREATE TABLE fields (
+  comp_code TEXT NOT NULL REFERENCES companies,
+  indust_code TEXT NOT NULL REFERENCES industries,
+  PRIMARY KEY(comp_code, indust_code)
+);
+
 INSERT INTO companies
   VALUES ('apple', 'Apple Computer', 'Maker of OSX.'),
-         ('ibm', 'IBM', 'Big blue.');
+         ('ibm', 'IBM', 'Big blue.'),
+         ('wellfarg', 'Wells Fargo', 'National bank.'),
+         ('turbtax', 'Turbo Tax', 'Do your own taxes fast.');
 
 INSERT INTO invoices (comp_Code, amt, paid, paid_date)
   VALUES ('apple', 100, false, null),
          ('apple', 200, false, null),
          ('apple', 300, true, '2018-01-01'),
          ('ibm', 400, false, null);
+
+INSERT INTO industries
+  VALUES ('acct', 'Accounting'), 
+         ('code', 'Coding'), 
+         ('fin', 'Finance');
+
+INSERT INTO fields
+  VALUES ('apple', 'code'),
+         ('ibm', 'code'),
+         ('wellfarg', 'fin'),
+         ('turbtax', 'acct');
